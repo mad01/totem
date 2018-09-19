f848411c255cfd26f0cab5d193ee03ef026d74a5
Alexander Brandstedt <alexander.brandstedt@ericsson.com>
2018-09-19 09:50:58 +0200

    remove todos that are done

--- main.go
+++ main.go
@@ -13,5 +13,3 @@ func main() {
 }
 
 // todo: adding option to limit to only one namespace with rolebinding not cluster role binding only
-// todo: change internal config of users to be map since list is O(n) and map is O(1)
-// todo: bug: when internaly running the service in a cluster the server address is the internal, need to use the dns name to get it to work
